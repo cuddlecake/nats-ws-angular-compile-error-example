3040b1013863722568803b20248c73c5bbb78985
Alexander Sawtschuk <sawtschuk.alex@gmail.com>
2021-01-13 12:05:47 +0100

    use connect to demonstrate compiling issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { connect } from 'nats.ws';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  ngOnInit() {
+	  connect()
+  }
 }
